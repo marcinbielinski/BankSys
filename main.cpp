ff858989f9b380569aa84208989cc1b1cb487c17
Sean Lock <howlempyrean@gmail.com>
2021-07-25 14:07:04 +0200

    fixed statics

--- main.cpp
+++ main.cpp
@@ -5,42 +5,46 @@
 #include <algorithm>
 
 struct Account {
-    int cash;
-
     Account(std::string fn, std::string ln, int money)
             : first_name(std::move(fn)), last_name(std::move(ln)), cash(money) {}
 
 
-    static std::string getFirstName(const Account& account)
+    std::string getFirstName()
     {
-        return account.first_name;
+        return first_name;
     }
 
-    static std::string getLastName(const Account& account)
+    std::string getLastName()
     {
-        return account.last_name;
+        return last_name;
     }
 
-    static std::string getID(const Account &account)
+    std::string getID()
     {
-        return account.id;
+        return id;
     }
 
-    static std::string getEmail(const Account& account)
+    std::string getEmail()
     {
-        return account.email;
+        return email;
     }
 
-    static int getCash(const Account& account)
+    int& getCash()
     {
-        return account.cash;
+        return cash;
     }
+
+//    int& addCash()
+//    {
+//
+//    }
 
 private:
     std::string first_name;
     std::string last_name;
     std::string id{first_name + " " + last_name};
     std::string email{first_name + "." + last_name + "@gmail.com"};
+    int cash;
 };
 
 class Bank {
@@ -56,46 +60,46 @@ public:
     void delAccount(std::string accountID) {
         the_bank.erase(std::remove_if(
                 begin(the_bank), end(the_bank),
-                [&accountID](auto &acc) { return Account::getID(acc) == accountID; }), end(the_bank));
+                [&accountID](auto& acc) { return acc.getID() == accountID; }), end(the_bank));
     }
 
     int accountBalance(std::string accountID) {
         auto result = std::find_if(begin(the_bank), end(the_bank),
-                                   [&accountID](auto &acc) { return Account::getID(acc) == accountID; });
+                                   [&accountID](auto& acc) { return acc.getID() == accountID; });
 
         (result != std::end(the_bank))
         ? std::cout << "The cash of this account: " <<
-                    Account::getID(*result) << " is: " << Account::getCash(*result) << '\n'
+                                                    result->getID() << " is: " << result->getCash() << '\n'
         : std::cout << "This account doesn't exist." << std::endl;
 
-        return Account::getCash(*result);
+        return result->getCash();
     }
 
     void transferCash(std::string accountID1, std::string accountID2, int amount)
     {
         auto sender = std::find_if(begin(the_bank), end(the_bank),
-                                   [&accountID1](auto& acc) { return Account::getID(acc) == accountID1;});
+                                   [&accountID1](auto& acc) { return acc.getID() == accountID1;});
 
         (sender != end(the_bank))
         ? std::cout << "Attempting the wire transfer... Please wait. \n"
         : std::cout << "The account you're trying to wire from doesn't exist." << std::endl;
 
         auto recipient = std::find_if(begin(the_bank), end(the_bank),
-                                      [&accountID2](auto& acc) { return Account::getID(acc) == accountID2;});
+                                      [&accountID2](auto& acc) { return acc.getID() == accountID2;});
 
         (recipient != end(the_bank))
-        ? std::cout << "Successfully wired: " << amount << " from: " << Account::getID(*sender)
-                                                        << " to: "   << Account::getID(*recipient)  << '\n'
+        ? std::cout << "Successfully wired: " << amount << " from: " << sender->getID()
+                                                        << " to: "   << recipient->getID()  << '\n'
         : std::cout << "This account doesn't exist." << std::endl;
 
-        sender->cash -= amount;
-        recipient->cash += amount;
+        sender->getCash() -= amount;
+        recipient->getCash() += amount;
     }
 
     int bankReserve() {
         int result;
         for (auto &elem : the_bank) {
-            reserve.emplace_back(Account::getCash(elem));
+            reserve.emplace_back(elem.getCash());
         }
         result = std::accumulate(begin(reserve), end(reserve), 0);
 
@@ -104,22 +108,11 @@ public:
     }
 
     void printBank() {
-        for (const auto &elem : the_bank) {
-            std::cout << "Name: " << Account::getFirstName(elem) << " Surname: " << Account::getLastName(elem)
-                      << " Money: " << Account::getCash(elem) << " E-mail: " << Account::getEmail(elem) << std::endl;
+        for (auto elem : the_bank) {
+            std::cout << "Name: " << elem.getFirstName() << " Surname: " << elem.getLastName()
+                      << " Money: " << elem.getCash() << " E-mail: " << elem.getEmail() << std::endl;
         }
     }
-
-private:
-    static void chargeMe(const Account& account, int amount)
-    {
-        Account::getCash(account) -= amount;
-    }
-    static void addMe(const Account& account, int amount)
-    {
-        Account::getCash(account) += amount;
-    }
-
 };
 
 int main() {
